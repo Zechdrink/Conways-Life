cdd56be00f6a7edbc1442f93f8f36bfae32fbae3
Zech Drinkhall <zechdrink@aol.com>
2019-07-15 17:41:44 -0700

    created my Canvas with cells

--- conway/src/main/Canvas.js
+++ conway/src/main/Canvas.js
@@ -1,14 +1,45 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 
+   
 
 
 const Canvas = (props) => {
 
-    const [ width, setWidth ] = useState(600);
-    const [ height, setHeight ] = useState(600);
+    const [ width, setWidth ] = useState(800);
+    const [ height, setHeight ] = useState(800);
     const canvasRef = useRef(null);
-    console.log(width)
+
+    let canvas;
+    let context;
+
+    const drawGrid = (width, height) => {
+        let cellWidth = width * .01;
+        let cellHeight = height * .01;
+      
+        for (let x =0; x <= width; x += cellWidth)
+        {
+            context.moveTo(0 + x, 0);
+            context.lineTo(x, height);
+        }
+
+         for (let x =0; x <= height; x += cellHeight)
+        {
+            context.moveTo(0, x);
+            context.lineTo(width, x);
+        }
+        context.strokeStyle = "black";
+        context.stroke();
+    }
+
+    useEffect(() => {
+        canvas = canvasRef.current;
+        context = canvas.getContext("2d")
+        drawGrid(width, height);  
+    })
+
+    
+
 
     return ( 
         <div>
@@ -16,6 +47,7 @@ const Canvas = (props) => {
              <canvas ref = {canvasRef} width={width} height={height} />
         </div>
      );
+
 }
  
 export default Canvas;
